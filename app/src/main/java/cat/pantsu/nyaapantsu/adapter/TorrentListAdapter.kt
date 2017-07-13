28d845d7d4515fd1b21ca2231c9a58544bf99f29
akuma06 <demon.akuma06@gmail.com>
2017-07-13 22:27:25 +0200

    Resolving issue
    SetOnClickListener was good. You just had to use the position variable since onclick only gives view as argument.
    
    Plus I changed backgroundcolor to cardbackgroundcolor. So we have the space between card when a torrent is trusted, aplus or remake

--- app/src/main/java/cat/pantsu/nyaapantsu/adapter/TorrentListAdapter.kt
+++ app/src/main/java/cat/pantsu/nyaapantsu/adapter/TorrentListAdapter.kt
@@ -2,7 +2,9 @@ package cat.pantsu.nyaapantsu.adapter
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -36,14 +38,14 @@ class TorrentListAdapter(var context: Context, torrentList: LinkedList<Torrent>)
         holder?.uploader?.text = item.username
         holder?.stats?.text = "S: " + item.seeders + " L: " + item.leechers
 
-        holder?.itemView?.setOnClickListener { i ->
-             context.startActivity<TorrentActivity>("position" to i, "type" to "search")
-        }//FIXME: this must be fixed
-
+        holder?.cardview?.setOnClickListener { _ ->
+             context.startActivity<TorrentActivity>("position" to position, "type" to "search")
+        }
+        
         when (item.status) {
-            2 -> holder?.itemView?.backgroundColor = ContextCompat.getColor(context, R.color.colorRemake)
-            3 -> holder?.itemView?.backgroundColor = ContextCompat.getColor(context, R.color.colorTrusted)
-            4 -> holder?.itemView?.backgroundColor = ContextCompat.getColor(context, R.color.colorAPlus)
+            2 -> holder?.cardview?.cardBackgroundColor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) context.resources.getColorStateList(R.color.colorRemake, null) else context.resources.getColorStateList(R.color.colorRemake)
+            3 -> holder?.cardview?.cardBackgroundColor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) context.resources.getColorStateList(R.color.colorTrusted, null) else context.resources.getColorStateList(R.color.colorTrusted)
+            4 -> holder?.cardview?.cardBackgroundColor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) context.resources.getColorStateList(R.color.colorAPlus, null) else context.resources.getColorStateList(R.color.colorAPlus)
         }
     }
 
@@ -65,5 +67,6 @@ class TorrentListAdapter(var context: Context, torrentList: LinkedList<Torrent>)
         val label: TextView = view.find<TextView>(R.id.label)
         val stats: TextView = view.find<TextView>(R.id.stats)
         val uploader: TextView = view.find<TextView>(R.id.uploader)
+        val cardview: CardView = view.find<CardView>(R.id.cardview)
     }
 }

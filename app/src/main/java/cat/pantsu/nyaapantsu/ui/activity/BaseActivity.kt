52f73954583290527d4a2cd8342911dc388949be
ltype <920687062@qq.com>
2017-07-22 23:22:40 +0800

    Add nyanpass voice

--- app/src/main/java/cat/pantsu/nyaapantsu/ui/activity/BaseActivity.kt
+++ app/src/main/java/cat/pantsu/nyaapantsu/ui/activity/BaseActivity.kt
@@ -1,8 +1,11 @@
 package cat.pantsu.nyaapantsu.ui.activity
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.MenuItem
+import cat.pantsu.nyaapantsu.R
 
 /**
  * Created by ltype on 2017/7/11.
@@ -16,10 +19,24 @@ abstract class BaseActivity: AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_pets, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // handle arrow click here
-        if (item.itemId == android.R.id.home && this !is HomeActivity) {
-            finish() // close this activity and return to preview activity (if there is any)
+        when (item.itemId) {
+            android.R.id.home -> {
+                if (this !is HomeActivity) finish()
+            }
+            R.id.menu_play -> {
+                //TODO support sukebei mode
+                val player = MediaPlayer.create(applicationContext, R.raw.nyanpass)
+                player.start()
+                player.setOnCompletionListener {
+                    player.release()
+                }
+            }
         }
 
         return super.onOptionsItemSelected(item)
